5aeecf887bcf48bab19220487c9a08a6bd6edc12
Gabriel CabreraQ <gabriel.cabrera.q@usach.cl>
2024-12-08 22:56:43 -0300

    ini repo with spring maven

---
+++ Hito3_Fingeso/src/main/java/com/easywheels/EasyWheelsApplication.java
@@ -0,0 +1,13 @@
+package com.easywheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWheelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWheelsApplication.class, args);
+	}
+
+}
---
+++ Hito3_Fingeso/src/test/java/com/easywheels/EasyWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easywheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
