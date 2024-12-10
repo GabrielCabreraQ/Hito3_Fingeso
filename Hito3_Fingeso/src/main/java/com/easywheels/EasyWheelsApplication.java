0b2fd03ea291d0a030e12131781342276417194d
willer0 <williams.jimenez@usach.cl>
2024-12-09 21:43:24 -0300

    test

--- Hito3_Fingeso/src/main/java/com/easywheels/EasyWheelsApplication.java
+++ Hito3_Fingeso/src/main/java/com/easywheels/EasyWheelsApplication.java
@@ -3,6 +3,7 @@ package com.easywheels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// probando si puedo realizar modificaciones
 @SpringBootApplication
 public class EasyWheelsApplication {
 
